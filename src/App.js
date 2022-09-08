351a164cbe2c8d2e9301005ee1bce2cff6d3fe6f
niralisikligar <sikligars02@gmail.com>
2022-09-08 13:23:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Navbar from "./Component/Navbar";
+import Home from "./Component/Home";
+import AddInterest from "./Component/AddInterest";
+import EditInterest from "./Component/EditInterest";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/add/:id" element={<AddInterest />} />
+        {/* <Route path="/add" element={<AddInterest />} /> */}
+        <Route path="/edit/:id" element={<EditInterest />} />
+      </Routes>
+    </>
   );
 }
 
